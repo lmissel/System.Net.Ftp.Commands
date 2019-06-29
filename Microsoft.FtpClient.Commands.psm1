68b928a750bea1340e7890dad67ac606d846f82d
lmissel <39304706+lmissel@users.noreply.github.com>
2019-06-29 15:09:34 +0200

    Summary Update

--- Microsoft.FtpClient.Commands.psm1
+++ Microsoft.FtpClient.Commands.psm1
@@ -159,7 +159,7 @@ function Get-FtpDirectory
             $StreamReader.ReadToEnd()
 
             #Ausgabe abschließen
-            Write-Host "List complete."
+            Write-Host "Auflistung komplett."
 
         }
         catch [System.UriFormatException]
@@ -199,6 +199,83 @@ function Get-FtpFile
 		[Switch]$UseBinary = $False,
 		[Switch]$UsePassive = $False
     )
+
+    Begin
+    {
+        [System.IO.Stream] $Stream = $null
+        [System.IO.FileStream] $FileStream = $null
+        [System.Net.FtpWebResponse] $FtpWebResponse = $null
+        [System.Net.FtpWebRequest] $FtpWebRequest = $null
+    }
+
+    Process
+    {
+        try
+        {
+            # Request erzeugen
+            $FtpWebRequest = [System.Net.FtpWebRequest]::Create($uri)
+
+            if ($Credentials)
+            {
+                $FtpWebRequest.Credentials = $Credentials
+            }
+
+            $FtpWebRequest.EnableSsl = $EnableSsl
+            $FtpWebRequest.KeepAlive = $Credentials
+            $FtpWebRequest.UseBinary = $UseBinary
+            $FtpWebRequest.UsePassive = $UsePassive
+
+            # Ausführende Aktion festlegen
+            $FtpWebRequest.Method = [System.Net.WebRequestMethods+Ftp]::DownloadFile
+            $FtpWebResponse = [System.Net.FtpWebResponse] $FtpWebRequest.GetResponse()
+            $Stream = $FtpWebResponse.GetResponseStream()
+
+            # Dateinamen extrahieren
+            $SaveFileDialog = [System.Windows.Forms.SaveFileDialog]::new()
+            $SaveFileDialog.FileName = [System.IO.Path]::GetFileName($FtpWebRequest.RequestUri.LocalPath)
+            if ($SaveFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::Ok)
+            {
+                # Öffnen der Zieldatei
+                $FileStream = [System.IO.File]::Create($SaveFileDialog.FileName)
+                [byte[]] $buffer = [byte[]]::new(1024)
+                [int] $byteRead = 0
+
+                # Einlesen und in Datei kopieren
+                while ($true)
+                {
+                    $byteRead = $Stream.Read($buffer, 0, $buffer.Length)
+                    if ($byteRead -eq 0)
+                    {
+                        break
+                    }
+                    $Stream.Write($buffer, 0, $byteRead)
+                }
+
+                Write-Host "Download komplett."
+            }
+        }
+        catch [System.UriFormatException]
+        {
+            # Fehlermeldung ausgeben
+        }
+        catch [System.Net.WebException]
+        {
+            # Fehlermeldung ausgeben
+        }
+        catch [System.IO.IOException]
+        {
+            # Fehlermeldung ausgeben
+        }
+        finally
+        {
+            if ($Stream) { $Stream.Close() }
+            if ($FileStream) { $FileStream.Close() }
+        }
+    }
+
+    End
+    {
+    }
 }
 
 # Remove File
@@ -216,4 +293,51 @@ function Remove-FtpFile
 		[Switch]$UseBinary = $False,
 		[Switch]$UsePassive = $False
     )
+
+    Begin
+    {
+        [System.Net.FtpWebResponse] $FtpWebResponse = $null
+        [System.Net.FtpWebRequest] $FtpWebRequest = $null
+    }
+
+    Process
+    {
+        try
+        {
+            # Request erzeugen
+            $FtpWebRequest = [System.Net.FtpWebRequest]::Create($uri)
+
+            if ($Credentials)
+            {
+                $FtpWebRequest.Credentials = $Credentials
+            }
+
+            $FtpWebRequest.EnableSsl = $EnableSsl
+            $FtpWebRequest.KeepAlive = $Credentials
+            $FtpWebRequest.UseBinary = $UseBinary
+            $FtpWebRequest.UsePassive = $UsePassive
+
+            # Ausführende Aktion festlegen
+            $FtpWebRequest.Method = [System.Net.WebRequestMethods+Ftp]::DeleteFile
+            $FtpWebResponse = [System.Net.FtpWebResponse] $FtpWebRequest.GetResponse()
+
+            Write-Host "Datei wurde gelöscht!"
+        }
+        catch [System.UriFormatException]
+        {
+            # Fehlermeldung ausgeben
+        }
+        catch [System.Net.WebException]
+        {
+            # Fehlermeldung ausgeben
+        }
+        finally
+        {
+            if($FtpWebResponse) { $FtpWebResponse.Close() }
+        }
+    }
+
+    End
+    {
+    }
 }
